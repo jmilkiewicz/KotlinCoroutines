3fdf514edbf80e614061441a1437b0c46b5613f0
Jakub Milkiewicz <jmilkiewicz@softmil.pl>
2024-05-30 16:22:01 +0200

    rename

--- src/main/kotlin/LimitParallelism2.kt
+++ src/main/kotlin/LimitParallelism2.kt
@@ -1,7 +1,6 @@
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -9,12 +8,12 @@ import kotlinx.coroutines.withContext
 var mycounter = 0
 val mysingleThreadDispatcher: CoroutineDispatcher = Dispatchers.IO.limitedParallelism(1)
 fun main(): Unit = runBlocking {
-    robCos(100000) { mycounter++ }
+    doSth(100000) { mycounter++ }
     println(mycounter)
 }
 
 
-suspend fun robCos(count: Int, action: (Unit) -> Unit) = coroutineScope {
+suspend fun doSth(count: Int, action: (Unit) -> Unit) = coroutineScope {
     withContext(Dispatchers.IO) {
         repeat(count) {
             launch(mysingleThreadDispatcher) { action.invoke(Unit) }
