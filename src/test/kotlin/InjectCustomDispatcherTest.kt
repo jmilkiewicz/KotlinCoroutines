7a838d5bacfde38317466e0709a7bbd4f3248fbc
Jakub Milkiewicz <jmilkiewicz@softmil.pl>
2024-05-31 16:21:57 +0200

    reproducing case in which we need to manually advance time in tests

--- src/test/kotlin/InjectCustomDispatcherTest.kt
+++ src/test/kotlin/InjectCustomDispatcherTest.kt
@@ -3,17 +3,16 @@ package coroutines.recipes.mapasync
 
 import coroutines.starting.showuserdata.Friend
 import coroutines.starting.showuserdata.Profile
+import coroutines.starting.showuserdata.User
 import coroutines.starting.showuserdata.UserDataRepository
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runTest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
-import kotlin.coroutines.CoroutineContext
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 
 class InjectCustomDispatcherTest {
@@ -22,18 +21,33 @@ class InjectCustomDispatcherTest {
         private val repo: UserDataRepository,
         private val dispatcher: CoroutineDispatcher = Dispatchers.IO
     ) {
-        suspend fun fetchUserData(): coroutines.starting.showuserdata.User = withContext(dispatcher) {
+        suspend fun fetchUserData(): User = withContext(dispatcher) {
             val name = async { repo.getName() }
             val friends = async { repo.getFriends() }
             val profile = async { repo.getProfile() }
-            coroutines.starting.showuserdata.User(
+            User(
                 name = name.await(), friends = friends.await(), profile = profile.await()
             )
+        }
+
+
+        fun nonSuspend(): Deferred<User> {
+            val customScope = CoroutineScope(dispatcher + SupervisorJob())
+            return customScope.async {
+                val name = async { repo.getName() }
+                val friends = async { repo.getFriends() }
+                val profile = async { repo.getProfile() }
+                User(
+                    name = name.await(), friends = friends.await(), profile = profile.await()
+                )
+            }
+
+
         }
     }
 
     companion object {
-        val sampleUser = coroutines.starting.showuserdata.User(
+        val sampleUser = User(
             name = "Ben",
             friends = listOf(Friend("some-friend-id-1")),
             profile = Profile("Example description")
@@ -72,6 +86,28 @@ class InjectCustomDispatcherTest {
         val fetchUserData = fetchUserUseCase.fetchUserData()
 
         assertThat(fetchUserData, `is`(sampleUser))
+        assertThat(currentTime, `is`(1500))
+    }
+
+
+    @OptIn(ExperimentalStdlibApi::class)
+    @Test
+    fun callingNonSuspensionFunction() = runTest{
+        val testDispatcher = currentCoroutineContext().get(CoroutineDispatcher.Key)!!
+
+        val fetchUserUseCase = FetchUserUseCase(FakeDelayedUserDataRepository(), testDispatcher)
+
+        val fetchUserData = fetchUserUseCase.nonSuspend()
+
+
+        //!!! when we call non suspending function which build its own scope we need to give it our own test dispatcher
+        // with virtual time + we need to manually advance time!!!
+        advanceTimeBy(1)
+        assertThat(fetchUserData.isCompleted, `is`(false))
+
+        advanceUntilIdle()
+
+        assertThat(fetchUserData.isCompleted, `is`(true))
         assertThat(currentTime, `is`(1500))
     }
 
